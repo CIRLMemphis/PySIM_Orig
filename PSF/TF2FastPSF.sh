dbd2dcd379af433db60a8e9944583578bd21e8ef
Cong Van <van.congtuanson@gmail.com>
2020-01-30 16:40:02 -0600

    Add tensorflow-based implementation of PSF and aberration-fixed test.

---
+++ PSF/TF2FastPSF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition gpuq
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --time=0-1:00:00
+
+# run matlab program via the run_matlab script
+python TF2FastPSF.py
