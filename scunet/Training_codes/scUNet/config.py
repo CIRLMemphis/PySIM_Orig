82711aa28a605abfbbe46d701d2b64187d804906
Cong <cvan@memphis.edu>
2020-06-09 06:40:11 -0500

    modify config.py in scUnet to use 1002 datasets.

--- scunet/Training_codes/scUNet/config.py
+++ scunet/Training_codes/scUNet/config.py
@@ -1,6 +1,6 @@
 out_dir = '../../../../out/pred/' #output directory
 inp_fname = '../../../../in/inFairSIM'
 out_fname = '../../../../out/outFairSIM'
-limit = 500 #How many image files we want to train
+limit = 1002 #How many image files we want to train
 Nthe = 3
 Nphi = 5
